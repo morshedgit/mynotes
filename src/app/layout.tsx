27a237ed19cc5dde4f4be66ff8903142f85ce8c7
Sadeq Morshedloo <smorshedloo@cercacor.com>
2024-06-17 16:52:11 -0700

    Init: cleans up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header>My Notes</header>
+        {children}
+      </body>
     </html>
   );
 }
