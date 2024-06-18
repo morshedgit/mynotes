22e7eb3751af8abe942da430210fbd3360a496cb
Sadeq Morshedloo <smorshedloo@cercacor.com>
2024-06-18 11:40:14 -0700

    style: improves ux

--- src/components/add-button/AddButton.tsx
+++ src/components/add-button/AddButton.tsx
@@ -1,10 +1,16 @@
 "use client";
-import { useRouter } from "next/navigation";
+import useNotesStore from "@/state/store";
+import { useParams, useRouter } from "next/navigation";
 
 const AddButton = () => {
+  const params = useParams();
   const router = useRouter();
 
+  const { notes } = useNotesStore();
+  const { noteId: id } = params as { noteId: string };
+
   const handleAddClick = () => {
+    if (id && id in notes && notes[id].text === "") return;
     const noteId = Math.random().toString().slice(2);
     router.push(`/${noteId}`);
   };
--- src/components/notes/list.tsx
+++ src/components/notes/list.tsx
@@ -1,15 +1,21 @@
 "use client";
 import useNotesStore from "@/state/store";
 import Link from "next/link";
+import { TrashIcon } from "@radix-ui/react-icons";
 
 const NoteList = () => {
-  const { notes } = useNotesStore();
+  const { notes, deleteNote } = useNotesStore();
 
   return (
     <ul className="p-2 h-full overflow-y-auto">
       {Object.entries(notes).map(([id, note]) => (
-        <li key={id} className="truncate text-black border-b-2 py-2">
-          <Link href={`/${note.id}`}>{note.text}</Link>
+        <li key={id} className="border-b-2 py-2 grid grid-cols-[1fr_auto] ">
+          <Link href={`/${note.id}`} className="truncate text-black min-w-0">
+            {note.text}
+          </Link>
+          <button type="button" onClick={() => deleteNote(id)}>
+            <TrashIcon />
+          </button>
         </li>
       ))}
     </ul>
